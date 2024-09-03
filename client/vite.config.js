d868eb185a082915addf77a2bd8930b22caece52
Isaac Katana <162148948+isaackatana@users.noreply.github.com>
2024-09-03 20:45:00 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://isaac-katana-fdnq.onrender.com',
         changeOrigin: true,
         },
       },
