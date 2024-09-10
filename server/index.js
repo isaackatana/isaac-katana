6a186fa1e6c6cd2e981c9ac2d4e561c8e4fae082
Isaac Katana <162148948+isaackatana@users.noreply.github.com>
2024-09-10 23:41:41 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
